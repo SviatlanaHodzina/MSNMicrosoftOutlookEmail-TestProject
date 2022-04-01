71991123d57843c9fa99976030951d422e81e56e
SviatlanaHodzina <102725239+SviatlanaHodzina@users.noreply.github.com>
2022-04-01 13:33:57 +0300

    deleted comments to CreatePasswordForNewEmailAccountTest

--- src/test/java/org/microsoft/MSNOutlook/tests/CreatePasswordForNewEmailAccountTest.java
+++ src/test/java/org/microsoft/MSNOutlook/tests/CreatePasswordForNewEmailAccountTest.java
@@ -24,19 +24,6 @@ public class CreatePasswordForNewEmailAccountTest extends RequiredConditions {
         String actualNextProfilePageTitleWithCreatedPassword = new CreateAPasswordPage().createAPassword(account).getProfilePageTitle();
         String expectedNextProfilePageTitleWithCreatedPassword = "What's your name?";
 
-//        Could not locate the element of the password placeholder's message, so I use another attribute to claim the test passes
-//        String actualMessageInPasswordPlaceholder = new CreateAPasswordPage().createPasswordMessageInPlaceholder();
-//        String expectedMessageInPasswordPlaceholder = "Create password";
-
-//        Could not locate the element of the "enter password" message, so I use another attribute to claim the test passes
-//        String actualEnterThePasswordMessage = new CreateAPasswordPage().enterThePasswordMessage();
-//        String expectedEnterThePasswordMessage = "Enter the password you would like to use with your account.";
-
-//        Could not locate the element of the available email account, so I use another attribute to claim the test passes
-//        String expectedAvailableEmailAccountOutlook = account.getEmailName().concat("@")
-//                .concat(account.getDomain());
-//        String alertMessageIfTestFails = "Created email names do not match";
-
         assertEquals(actualNextProfilePageTitleWithCreatedPassword, expectedNextProfilePageTitleWithCreatedPassword);
     }
 }
