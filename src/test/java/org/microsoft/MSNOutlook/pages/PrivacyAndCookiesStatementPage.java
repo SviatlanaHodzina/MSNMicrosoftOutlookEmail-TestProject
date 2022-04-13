b5c2012eb7e573440ab705ace1883bb7cb9597a7
Sviatlana Hodzina <102725239+SviatlanaHodzina@users.noreply.github.com>
2022-04-13 20:11:40 +0300

    added PrivacyAndCookiesStatementPage.java

---
+++ src/test/java/org/microsoft/MSNOutlook/pages/PrivacyAndCookiesStatementPage.java
@@ -0,0 +1,37 @@
+package org.microsoft.MSNOutlook.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PrivacyAndCookiesStatementPage extends AbstractPage {
+    Logger logger = LogManager.getRootLogger();
+
+    private final String PRIVACY_AND_COOKIES_STATEMENT_PAGE_CONTENT_ELEMENT_XPATH = "/html/body";
+
+    public PrivacyAndCookiesStatementPage (){
+        super();
+        PageFactory.initElements(driver, this);
+    }
+
+    @Override
+    public PrivacyAndCookiesStatementPage openPage() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath(PRIVACY_AND_COOKIES_STATEMENT_PAGE_CONTENT_ELEMENT_XPATH)));
+        logger.info("You are navigated to 'PRIVACY AND COOKIES STATEMENT PAGE'");
+        return this;
+    }
+    public String getPrivacyAndCookiesStatementPageContent() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath(PRIVACY_AND_COOKIES_STATEMENT_PAGE_CONTENT_ELEMENT_XPATH)));
+        return driver.findElement(By.xpath(PRIVACY_AND_COOKIES_STATEMENT_PAGE_CONTENT_ELEMENT_XPATH))
+                .getAttribute("textContent");
+    }
+}
